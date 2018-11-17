b38886451a91406b5fca876506c6ba956addce17
Nathan <nathan.butt@gmail.com>
2018-11-17 22:35:42 +0000

    Added tensorflow project

---
+++ cnn-games/cnn-game.py
@@ -0,0 +1,14 @@
+# Game generation CNN. For use in providing data to unity.
+# Nathan Butt
+# Student No - 16013327
+
+import tensorflow as tf
+import numpy as np
+
+
+
+def main():
+    print("Hello world. How are you today.")
+
+if __name__ == "__main__":
+    main()
