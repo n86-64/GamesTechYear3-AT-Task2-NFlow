fd132d1184a904d1e074d63390dfed39cfbd9ba5
Nathan <nathan.butt@gmail.com>
2018-12-10 10:37:49 +0000

    Finalised script for creating predictions.

--- AT-Task2/Assets/Scripts/Predictor.cs
+++ AT-Task2/Assets/Scripts/Predictor.cs
@@ -6,22 +6,71 @@ using System.IO;
 
 public class Predictor : MonoBehaviour
 {
-    private string pyOut = null;
+    // Predictions from the neural-network. 
+    public List<string> labels = new List<string>();
+    public List<float> predictions = new List<float>();
 
 	// Use this for initialization
 	void Start ()
     {
-        getPredictions("test.jpg");
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
+        //getPredictions("test.jpg");
+        // loadLabels("labels.txt");
 	}
 
-    // Gets the predictions from the array.
-    public string getPredictions(string imageFile)
+    // Load the labels file into memory.
+    public void loadLabels(string fileName)
+    {
+        labels.Clear();
+        predictions.Clear();
+
+        StreamReader fileReader = new StreamReader(Application.dataPath + "\\" + fileName);
+
+        if (fileReader != null)
+        {
+            while (!fileReader.EndOfStream)
+            {
+                labels.Add(fileReader.ReadLine());
+                predictions.Add(0.0f);
+            }
+        }
+
+        fileReader.Close();
+    }
+
+    // Performs a prediction. Stores the result in an array for later use.
+    public void predict(string fileName)
+    {
+        string prediction = getPredictions(fileName);
+        char strCharacter = '\0';
+        string valueString = "";
+        int labelIndex = 0;
+
+        for(int i = 0; i < prediction.Length; i++)
+        {
+            strCharacter = prediction[i];
+            if(strCharacter != '[' || strCharacter != ']')
+            {
+                if (strCharacter != ',')
+                {
+                    valueString += strCharacter;
+                }
+                else
+                {
+                    predictions[labelIndex] = float.Parse(valueString);
+                    labelIndex++;
+                }
+            }
+        }
+    }
+
+    // Returns a prediction value for a label.
+    float getPredictionValue(string label)
+    {
+        return predictions[labels.IndexOf(label)];
+    }
+
+    // Gets the predictions from the array utalising the neural-network.
+    string getPredictions(string imageFile)
     {
         // The pyhon process with supervising parameters.
         Process neuralNetwork = null;
@@ -41,4 +90,6 @@ public class Predictor : MonoBehaviour
         // Get the output.
         return reader.ReadToEnd();
     }
+
+    
 }
