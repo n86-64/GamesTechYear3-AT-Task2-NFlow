d26a3e5ea6da6b45f4192ef5d9a1c867caff53d3
Nathan <nathan.butt@gmail.com>
2019-01-18 13:44:55 +0000

    Added dead state

--- AT-Task2/Assets/Scripts/Characters/PlatformerCharacter.cs
+++ AT-Task2/Assets/Scripts/Characters/PlatformerCharacter.cs
@@ -7,6 +7,9 @@ public class PlatformerCharacter : MonoBehaviour {
     private Camera playerCam;
     private Animator characterAnimator;
     private Rigidbody characterBody;
+    private int health;
+    private bool dead;
+
 
     public GameObject viewTarget;
     public float runSpeed = 10.0f;
@@ -24,9 +27,21 @@ public class PlatformerCharacter : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if(health <= 0 && dead)
+        {
+            Dead();
+        }
+
+        if (characterAnimator.GetCurrentAnimatorStateInfo(0).IsName("Dead"))
+        {
+            characterAnimator.SetBool("Dead", false);
+        }
+
         Jump();
         Forward();
         Rotation();
+        
+        dead = Input.GetKeyDown(KeyCode.K); 
 	}
 
 
@@ -47,5 +62,11 @@ public class PlatformerCharacter : MonoBehaviour {
     {
         float val = Input.GetAxis("Horizontal");
         gameObject.transform.Rotate(new Vector3(0.0f, val, 0.0f));
+    }
+
+    void Dead()
+    {
+        // the character is dead.
+        characterAnimator.SetBool("Dead", true);
     }
 }
