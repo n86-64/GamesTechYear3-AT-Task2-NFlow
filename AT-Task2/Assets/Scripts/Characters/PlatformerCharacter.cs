6e4aaa78b0265818a542eb1a210e6549132a469f
Nathan <nathan.butt@gmail.com>
2019-01-18 12:47:06 +0000

    Created basic 3D character

---
+++ AT-Task2/Assets/Scripts/Characters/PlatformerCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerCharacter : MonoBehaviour {
+
+    private Camera playerCam;
+    private Animator characterAnimator;
+    private Rigidbody characterBody;
+
+    public GameObject viewTarget;
+    public float runSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerCam = gameObject.GetComponentInChildren<Camera>();
+        playerCam.transform.LookAt(viewTarget.transform);
+        playerCam.tag = "MainCamera";
+        characterAnimator = gameObject.GetComponent<Animator>();
+        characterBody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Jump();
+        Forward();
+	}
+
+
+    // Move forward
+    void Forward()
+    {
+        float axisVal = Input.GetAxis("Vertical");
+        characterAnimator.SetFloat("Speed", axisVal);
+        characterBody.velocity = gameObject.transform.forward * runSpeed * axisVal;
+    }
+
+    void Jump()
+    {
+        characterAnimator.SetBool("Jump", Input.GetKeyDown(KeyCode.Space));
+    }
+}
