1b24bdc755bb505661459461902bf0852a50f90b
Nathan <nathan.butt@gmail.com>
2019-02-22 23:54:42 +0000

    Created menu

---
+++ AT-Task2/Assets/Scripts/DeletionScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletionScript : MonoBehaviour
+{
+
+    public void deleteGameGenerator()
+    {
+        GameGenerator generator = FindObjectOfType<GameGenerator>();
+        if (generator)
+        {
+            Destroy(generator.gameObject);
+        }
+    }
+}
