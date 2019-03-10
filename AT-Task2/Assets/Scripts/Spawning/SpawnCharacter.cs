286378cd5c09945c30b2799aa6678184fb111fcd
Nathan <nathan.butt@gmail.com>
2019-03-10 14:51:43 +0000

    Added a spawn prefab and added it to new scene

---
+++ AT-Task2/Assets/Scripts/Spawning/SpawnCharacter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacter : MonoBehaviour {
+
+    public List<string> label;
+    public List<GameObject> characterPrefabs;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GameGenerator generator = FindObjectOfType<GameGenerator>();
+        int characterToSpawn = 0;
+
+        characterToSpawn = label.FindIndex(x => x == generator.generatedCharacter);
+        Instantiate(characterPrefabs[characterToSpawn], gameObject.transform.position, Quaternion.Euler(Vector3.zero));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void RespawnCharacter()
+    {
+
+    }
+}
